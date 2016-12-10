6969fb78dcb7b4e5fd2b4eae3b8071b8de700e78
Contratar <contratarsegurossalud@gmail.com>
2016-12-10 21:20:25 +0100

    Add files via upload

---
+++ js/functions.js
@@ -0,0 +1,8 @@
+function mostrar(id,id2) {
+    var theDetail = document.getElementById(id);
+        theDetail.style.display="block";
+
+var theDetail = document.getElementById(id2);
+    theDetail.style.display="none";
+    
+}
